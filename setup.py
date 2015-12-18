6bb6a77a7c408ea97aa1bd9341ea2490a602cb89
Ben Demaree <ben.demaree@ave81.com>
2015-12-18 15:25:49 -0600

    Update package information.
    Adds description, license, classifiers, etc. Also changes to double quotes for consistency.

--- setup.py
+++ setup.py
@@ -3,12 +3,24 @@ from setuptools import setup
 from gcloud_requests import __version__
 
 setup(
-    name='gcloud_requests',
+    name="gcloud_requests",
+    description="Thread-safe client functionality for gcloud-python via requests.",
     version=__version__,
-    packages=['gcloud_requests'],
+    url="https://github.com/LeadPages/gcloud_requests",
+    author="LeadPages",
+    license="MIT",
+    packages=["gcloud_requests"],
     install_requires=[
-        'gcloud==0.7.0',
-        'requests==2.7.0',
-        'certifi==2015.09.06.2'
+        "gcloud==0.7.0",
+        "requests==2.7.0",
+        "certifi==2015.09.06.2"
+    ],
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Libraries",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7"
     ]
 )
