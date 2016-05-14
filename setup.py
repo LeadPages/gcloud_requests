7539fe3dcaa2558a4852e0760a28283db9873a84
Ben Demaree <ben.demaree@ave81.com>
2016-05-13 20:28:58 -0500

    Pin gcloud to 0.12.1

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
     license="MIT",
     packages=["gcloud_requests"],
     install_requires=[
-        "gcloud==0.12.0",
+        "gcloud==0.12.1",
         "requests>=2.9.0,<3.0",
         "certifi==2015.09.06.2"
     ],
