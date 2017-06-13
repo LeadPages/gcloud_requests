ac8dd7c8df2923c4dcba25c8ca63eeb3cd54628a
Bogdan Popa <popa.bogdanp@gmail.com>
2017-06-13 12:01:08 +0300

    Bump version: 1.0.0 → 1.0.1

--- gcloud_requests/__init__.py
+++ gcloud_requests/__init__.py
@@ -2,4 +2,4 @@ from .proxy import RequestsProxy  # noqa
 from .datastore import DatastoreRequestsProxy, enter_transaction, exit_transaction  # noqa
 from .storage import CloudStorageRequestsProxy  # noqa
 
-__version__ = "1.0.0"
+__version__ = "1.0.1"
