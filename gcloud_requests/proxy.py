8e28fb0b44388de45a861e5c2d5eff6777f59c35
Bogdan Popa <popa.bogdanp@gmail.com>
2017-09-08 20:34:42 +0300

    turn off watcher

--- gcloud_requests/proxy.py
+++ gcloud_requests/proxy.py
@@ -11,8 +11,6 @@ from google.cloud.credentials import get_credentials
 from requests.packages.urllib3.util.retry import Retry
 from threading import local
 
-from .credentials_watcher import CredentialsWatcher
-
 _state = local()
 _refresh_status_codes = (401,)
 _max_refresh_attempts = 5
@@ -68,9 +66,6 @@ class RequestsProxy(object):
 
         self.logger = logger or logging.getLogger(type(self).__name__)
         self.credentials = credentials
-
-        if watcher:
-            self.credentials_watcher = CredentialsWatcher(credentials)
 
     def request(self, uri, method="GET", body=None, headers=None, redirections=5, connection_type=None, retries=0, refresh_attempts=0):   # noqa
         session = self._get_session()
